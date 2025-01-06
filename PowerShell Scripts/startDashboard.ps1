3c3e8cddafced371784ca1cae3a950ed6ab752a2
ASaidkh <118311424+ASaidkh@users.noreply.github.com>
2025-01-06 16:46:55 -0500

    Update startDashboard.ps1

--- PowerShell Scripts/startDashboard.ps1
+++ PowerShell Scripts/startDashboard.ps1
@@ -1,8 +1,8 @@
-$file_name = $args[0]
+X$file_name = $args[0]
 Write-Output "Starting dashboard: " + $file_name
 
 
 # define dashboard wsl command to start
-$execution_command = "wsl -d AlmaLinux-8 nohup python3 /home/NeoHPHTLogger/Code/dashboard.py /home/NeoHPHTLogger/Data/" + $file_name + ".csv" 
+$execution_command = "wsl -d AlmaLinux-8 nohup python3 EXECUTABLE_FILE_PATH DATA_FILE_PATH" + $file_name + ".csv" 
 
 Invoke-Expression $execution_command
