5373278bc475f3d80c841ff5388159b6d22b0061
ASaidkh <118311424+ASaidkh@users.noreply.github.com>
2025-01-06 16:46:18 -0500

    Update startCSV.ps1

--- PowerShell Scripts/startCSV.ps1
+++ PowerShell Scripts/startCSV.ps1
@@ -4,6 +4,6 @@ Write-Output "Starting CSV Program: " + $file_name
 
 
 # define wsl command to start CSV program
-$execution_command = 'wsl -d AlmaLinux-8 nohup /home/NeoHPHTLogger/Code/./DataTaker_FTP_CSV /var/log/xferlog ' + $file_name 
+$execution_command = 'wsl -d AlmaLinux-8 nohup EXECUTABLE_FILE_PATH /var/log/xferlog ' + $file_name 
 
 Invoke-Expression $execution_command
